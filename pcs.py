6ed7b6549de5818052988d62b50731bbd072a281
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-03-22 23:13:13 -0300

    Update pcs.py

--- pcs.py
+++ pcs.py
@@ -2,6 +2,7 @@ from socket import socket, AF_INET, SOCK_DGRAM
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
+# Função para enviar uma solicitação para o servidor
 def send_request(node_id, port, server_address, public_key):
     client_socket = socket(AF_INET, SOCK_DGRAM)
 
@@ -16,7 +17,9 @@ def send_request(node_id, port, server_address, public_key):
 
     client_socket.close()
 
+# Função principal para iniciar a comunicação entre os nós
 def main():
+    # Dicionário para armazenar as informações dos nós
     nodes = {
         'PC1': {'port': 5001, 'public_key': None},
         'PC2': {'port': 5002, 'public_key': None},
@@ -26,22 +29,21 @@ def main():
         'PC6': {'port': 5006, 'public_key': None},
     }
 
-    # Gerar chaves públicas para os nós
-    for node_id, config in nodes.items():
-        private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
-        config['public_key'] = private_key.public_key()
-
-    # Registro dos nós na Autoridade Certificadora
+    # Gerar chaves públicas para os nós e registrar na Autoridade Certificadora
     ac_address = ('localhost', 1234)
     for node_id, config in nodes.items():
-        client_socket = socket(AF_INET, SOCK_DGRAM)
+        # Gera uma chave privada e extrai a chave pública
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        config['public_key'] = private_key.public_key()
+
+        # Serializa a chave pública em formato PEM
         public_key_bytes = config['public_key'].public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
+
+        # Envia a chave pública para a Autoridade Certificadora
+        client_socket = socket(AF_INET, SOCK_DGRAM)
         register_message = f"REGISTER|{node_id}|{public_key_bytes.decode()}"
         client_socket.sendto(register_message.encode(), ac_address)
         data, _ = client_socket.recvfrom(2048)
@@ -53,5 +55,7 @@ def main():
     for node_id, config in nodes.items():
         send_request(node_id, config['port'], ac_address, config['public_key'])
 
+# Verifica se o script está sendo executado diretamente
 if __name__ == "__main__":
+    # Chama a função principal para iniciar a comunicação entre os nós
     main()
